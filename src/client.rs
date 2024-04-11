62ea4c0ddfd7306025228a88f9d87da50624b165
Floorey <lukas_enderle@web.de>
2024-04-11 21:55:10 +0200

    client code user input added

--- src/client.rs
+++ src/client.rs
@@ -4,17 +4,25 @@ use std::net::TcpStream;
 fn main() -> io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:7878")?;
 
-    // Sende Befehle an den Server
-    let commands = vec!["add_water", "release_water"];
+    println!("Client connect to server.");
 
-    for command in commands {
+
+    loop {
+        println!("Enter a command (add_water or release_water), or 'quit' to exit:");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+
+        let command = input.trim();
+        if command == "quit" {
+            break;
+        }
+
         stream.write_all(command.as_bytes())?;
-        println!("Command sent to server: {}", command);
+        println!("Command sent to server; {}", command);
 
         let mut response = String::new();
         stream.read_to_string(&mut response)?;
         println!("Server response: {}", response);
     }
-
     Ok(())
 }
